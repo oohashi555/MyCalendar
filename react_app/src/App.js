0b5ac009fb2c2aebacac9cb5e48e7474242f4215
oohashi <hanabi5479@gmail.com>
2024-08-14 16:07:50 +0900

    swr

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	const url = "http://localhost:8080/";
+	const [data, setData] = useState("");
+	async function getData() {
+		const response = await fetch(url);
+		const data = await response.json();
+		const str = JSON.stringify(data, null, ' ');
+		setData(str);
+	}
+	getData();
+	
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React app.</h1>
+      <pre>{data}</pre>
     </div>
   );
 }
--- src/main/java/com/mycalendar/controller/MyCalendarRestController.java
+++ src/main/java/com/mycalendar/controller/MyCalendarRestController.java
@@ -1,10 +1,14 @@
 package com.mycalendar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mycalendar.entity.Schedule;
 import com.mycalendar.repositories.ScheduleRepository;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class MyCalendarRestController {
@@ -12,8 +16,9 @@ public class MyCalendarRestController {
 	@Autowired
 	ScheduleRepository repository;
 	
-	@RequestMapping("/")
-	public String hello() {
-		return "";
+	@GetMapping("/")
+	@CrossOrigin
+	public Mono<Schedule> hello() {
+		return Mono.just(new Schedule());
 	}
 }
